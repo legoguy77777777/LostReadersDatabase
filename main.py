0fc9dedc6df4934102519b95556830f24f4b8e04
Lee <amvm8v@umsystem.edu>
2025-11-20 15:46:37 -0600

    import basics

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-#test
-print("hello world")
+# Lost Reader Database
+# Main function execution
+# CS2300 Final
+
+import sql_query.py as sq
+import time
+import os
+
+
+
+def Menu():
+	
+
+
+def main():
+	Initial_Setup()
+	Menu()
+
+
+if __name__ == "__main__":
+	main()
---
+++ sql_query.py
@@ -0,0 +1,5 @@
+import mysql.connector
+import random
+from enum import Enum
+
+
