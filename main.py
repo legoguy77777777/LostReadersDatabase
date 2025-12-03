49ada036fa2d290076a51cfba2c613bb21636d73
Ava <144729779+legoguy77777777@users.noreply.github.com>
2025-12-03 13:42:48 -0600

    Added functions to main.py
    Finished Show_Tables()
    Start Add_Tables()

--- main.py
+++ main.py
@@ -6,14 +6,114 @@ import sql_query.py as sq
 import time
 import os
 
+def Add_Tuples():
+    clear()
+    sq.Show_Database()
+    table = int(input("Which table # would you like to add to? [0 to exit]: "))
+    
+    while table < 0 or table > 9:
+        print("Please choose from the available table #")
+        sq.Show_Database()
+        table = int(input("Which table # would you like to see?: "))
+    match table:
+        case 0:
+            return
+        case 1:
+            name = input("Please enter your name: ")
+            password = input("Plase enter a password: ")
+            if password != 0:
+                authorization_no = 5
+                count = 0
+                for i in authorization_lists:
+                    count +=1
+                    print( "{}. ".format(count) + i)
+                while authorization_no not in range(0,4):
+                    authorization_no = int(input("Please enter authorization number:"))
+                
+                sq.Add_Librarian(authorization_no,password,name)
+		case 2:
+            name = input("Please enter your name: ")
+            password = input("Plase enter a password: ")
+            if password != 0:
+                authorization_no = 5
+                count = 0
+                for i in authorization_lists:
+                    count +=1
+                    print( "{}. ".format(count) + i)
+                while authorization_no not in range(0,4):
+                    authorization_no = int(input("Please enter authorization number:"))
+                
+                sq.Add_Patron(authorization_no,password,name,null)
+		case 3:
+            name = input("Please enter your name: ")
+            password = input("Plase enter a password: ")
+            if password != 0:
+                authorization_no = 5
+                count = 0
+                for i in authorization_lists:
+                    count +=1
+                    print( "{}. ".format(count) + i)
+                while authorization_no not in range(0,4):
+                    authorization_no = int(input("Please enter authorization number:"))
+                
+                sq.Add_Patron(authorization_no,password,name,null)
 
 
-def menu():
+def Show_Tables():
+    clear()
+    sq.Show_Database()
+    table = int(input("Which table # would you like to see? (enter 0 to quit): "))
 
+    while table < 0 or table > 4:
+        print("Please choose from the available table #")
+        sq.Show_Database()
+        table = int(input("Which table # would you like to see? (enter 0 to quit): "))
+    match table:
+        case 0:
+            clear(  )
+            return
+        case 1:
+            sq.Show_Librarians()
+            okay = input("press ENTER")
+            Show_Tables()
+        case 2:
+            sq.Show_Patrons()
+            okay = input("press ENTER")
+            Show_Tables()
+        case 3:
+            sq.Show_Media()
+            okay = input("press ENTER")
+            Show_Tables()
+        case 4:
+            sq.Show_Media_Books()
+            okay = input("press ENTER")
+            Show_Tables()
+		case 5:
+            sq.Show_Media_dvds()
+            okay = input("press ENTER")
+            Show_Tables()
+		case 6:
+            sq.Show_Media_item()
+            okay = input("press ENTER")
+            Show_Tables()
+		 case 7:
+            sq.Show_Locations()
+            okay = input("press ENTER")
+            Show_Tables()
+		 case 8:
+            sq.Show_Overdue_flags()
+            okay = input("press ENTER")
+            Show_Tables()
+		 case 9:
+            sq.Show_Waitlists()
+            okay = input("press ENTER")
+            Show_Tables()
+		
+        case 0:
+            return 0
 
 
 def main():
-	Initial_Setup()
 	Menu()
 
 
