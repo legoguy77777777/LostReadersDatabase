5a045fe75ab059d024414287bf86443bd5b89b12
Lee <amvm8v@umsystem.edu>
2025-12-04 02:20:54 -0600

    new sql query file

---
+++ query2.py
@@ -0,0 +1,291 @@
+import mysql.connector
+import random
+from enum import Enum
+
+#code notes :
+# entering "0" results in moving back one step
+# only 2 types of login accounts
+
+# connection to MySQL server
+connection = mysql.connector.connect(
+	host = "localhost",
+	user = "root",
+	password = "LostReadersDatabase",
+	database = "Library_Database_Phase3"
+)
+
+mycursor = connection.cursor()
+
+def login():
+	id_exists = 0
+	id_type = input("Please enter '1' for Patron Login or '2' for Librarian Login: ")
+	while id_exists == 0:
+		id = input("Please enter your ID (enter '0' to quit): ")
+		if id == '0':
+			quit()
+		if id_type == '1':
+			mycursor.execute("SELECT * FROM Patron WHERE Patron_id = (%s)", (id,))
+		if id_type == '2':
+			mycursor.execute("SELECT * FROM Librarian WHERE librarian_id = (%s)", (id,))
+
+def show_database():
+	print("Showing Database Tables: ")
+
+#printing Librarian table
+	print("Librarian:")
+	table = mycursor.execute("""
+		select *
+		from Librarian;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+	print(row)
+
+#printing Patron table
+	print("\nPatron:")
+	table = mycursor.execute("""
+		select *
+		from Patron;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+#printing Media table
+	print("\nMedia:")
+	table = mycursor.execute("""
+		select *
+		from Media;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+#printing Media_book table
+	print("\nMedia_book:")
+	table = mycursor.execute("""
+		select *
+		from Media_book;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+#printing Media_dvd table
+	print("\nMedia_dvd:")
+	table = mycursor.execute("""
+		select *
+		from Media_dvd;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+#printing Media_item table
+	print("\nMedia_item:")
+	table = mycursor.execute("""
+		select *
+		from Media_item;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+#printing Location table
+	print("\nLocation:")
+	table = mycursor.execute("""
+		select *
+		from Location;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+#printing Edit table
+	print("\nEdit:")
+	table = mycursor.execute("""
+		select *
+		from Edit;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+#printing Overdue_flag table
+	print("\nOverdue_flag:")
+	table = mycursor.execute("""
+		select *
+		from Overdue_flag;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+#printing Waitlist table
+	print("\nWaitlist:")
+	table = mycursor.execute("""
+		select *
+		from Waitlist;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+def show_patrons():
+	print("Patrons:")
+	table = mycursor.execute("""
+		select Patron_name, Member_id, Wishlist
+		from Patron;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+def show_librarians():
+#needs done
+
+def show_media():
+	print("Showing Media by Type:")
+
+# printing table of books
+	print("Books:")
+	table = mycursor.execute("""
+		select Media_book.Dewey_decimal_code, Media.Media_name, Media_book.Author, Media_book.Genre, Media.Availability, Medi$
+		from Media, Media_book
+		where Media.Dewey_decimal_code = Media_book.Dewey_decimal_code;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+# printing table of dvds
+	print("Dvds:")
+	table = mycursor.execute("""
+		select Media_dvd.Dewey_decimal_code, Media.Media_name, Media_dvd.Director, Media_dvd.Genre, Media.Availability, Media$
+		from Media, Media_dvd
+		where Media.Dewey_decimal_code = Media_dvd.Dewey_decimal_code;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+# printing table of items
+	print("Items:")
+	table = mycursor.execute("""
+		select Media_item.Dewey_decimal_code, Media.Media_name, Media.Availability, Media.Copies
+		from Media, Media_item
+		where Media.Dewey_decimal_code = Media_item.Dewey_decimal_code;
+		""")
+	table_info = mycursor.fetchall()
+	for row in table_info:
+		print(row)
+
+def show_media_attributes():
+#do we even need this? it is techically the search fuction
+
+        
+
+def show_wishlist():
+#needs done
+
+def show_waitlist():
+#needs done
+        
+def overdue_flag():
+#needs done
+
+def add_media():
+#needs done
+
+def add_patron():
+#needs done
+
+def add_librarian():
+#needs done
+
+def reserve_media():
+#needs done
+
+
+def checkout_media():
+#needs done
+
+
+def return_media():
+#needs done
+
+
+def edit_password():
+#needs done
+
+
+def edit_location():
+#needs done
+
+
+def edit_waitlist():
+#needs done
+
+
+def edit_wishlist():
+#needs done
+
+
+def delete_media():
+#needs done
+
+
+def delete_patron():
+#needs done
+
+
+def delete_librarian():
+#needs done
+
+        
+#aggregate functions below
+#media_type is 1, 2, or 3. 1 = book, 2 = dvd, and 3 = item
+def get_sum_media_type(media_type):
+# sum of books
+	if media_type == 1:
+		table = mycursor.execute("""
+			select sum(copies)
+			from Media, Media_book
+			where Media.Dewey_decimal_code = Media_book.Dewey_decimal_code;
+			""")
+		result = mycursor.fetchone()[0]
+
+# sum of dvds
+	elif media_type == 2:
+		table = mycursor.execute("""
+			select sum(copies)
+			from Media, Media_dvd
+			where Media.Dewey_decimal_code = Media_dvd.Dewey_decimal_code;
+			""")
+		result = mycursor.fetchone()[0]
+
+# sum of items
+	elif media_type == 3:
+		table = mycursor.execute("""
+			select sum(copies)
+			from Media, Media_item
+			where Media.Dewey_decimal_code = Media_item.Dewey_decimal_code;
+			""")
+		result = mycursor.fetchone()[0]
+	return result
+
+def get_sum_media():
+	sum_books = get_sum_media_type(1)
+	sum_dvds = get_sum_media_type(2)
+	sum_items = get_sum_media_type(3)
+	total = sum_books + sum_dvds + sum_items
+	print("The total number of media (books/dvds/items) in the library is:", total)
+
+def get_sum_available():
+	table = mycursor.execute("""
+		select sum(Availability)
+		from Media;
+		""")
+	total = mycursor.fetchone()[0]
+	print("The total number of media (books/dvds/items) available for checkout in the library is:", total)
+
+def search_media():
+	#needs done
