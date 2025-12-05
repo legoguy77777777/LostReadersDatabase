88d00b2d9d40b37631a1c43cbb1860589220bfcf
Ava <144729779+legoguy77777777@users.noreply.github.com>
2025-12-05 16:29:54 -0600

    Added inserting media, need to finish making only for librarians

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def Add_Tuples():
                     count +=1
                     print( "{}. ".format(count) + i)
                 while authorization_no not in range(0,4):
-                    authorization_no = int(input("Please enter authorization number:"))
+                    authorization_no = int(input("Please enter ID number:"))
                 
                 sq.Add_Librarian(authorization_no,password,name)
 		case 2:
@@ -41,22 +41,49 @@ def Add_Tuples():
                     count +=1
                     print( "{}. ".format(count) + i)
                 while authorization_no not in range(0,4):
-                    authorization_no = int(input("Please enter authorization number:"))
+                    authorization_no = int(input("Please enter ID number:"))
                 
                 sq.Add_Patron(authorization_no,password,name,null)
 		case 3:
-            name = input("Please enter your name: ")
-            password = input("Plase enter a password: ")
-            if password != 0:
-                authorization_no = 5
-                count = 0
-                for i in authorization_lists:
-                    count +=1
-                    print( "{}. ".format(count) + i)
-                while authorization_no not in range(0,4):
-                    authorization_no = int(input("Please enter authorization number:"))
-                
-                sq.Add_Patron(authorization_no,password,name,null)
+			id = input("Please enter your librarian ID: ")
+            #if ID in librarian IDs
+            Media_name = input("Plase enter a media's title/name: ")
+			dewey_dec = input("Plase enter a media's dewey decimal code: ")
+			summary = input("Plase enter a media's summary: ")
+			copies = input("Plase enter number of copies of media that will be stocked: ")
+            sq.Add_Media(dewey_dec,summary,Media_name,1,copies,null,null)
+		case 4:
+			id = input("Please enter your librarian ID: ")
+            #if ID in librarian IDs
+            Media_name = input("Plase enter a media's title/name: ")
+			dewey_dec = input("Plase enter a media's dewey decimal code: ")
+			summary = input("Plase enter a media's summary: ")
+			copies = input("Plase enter number of copies of media that will be stocked: ")
+            sq.Add_Media(dewey_dec,summary,Media_name,1,copies,null,null)
+			Author = input("Plase enter the book's author: ")
+			    Genre = input("Plase enter the book's genre: ")
+			    sq.Add_Media_Book(Author,Genre,dewey_dec)
+		case 5:
+			id = input("Please enter your librarian ID: ")
+            #if ID in librarian IDs
+            Media_name = input("Plase enter a media's title/name: ")
+			dewey_dec = input("Plase enter a media's dewey decimal code: ")
+			summary = input("Plase enter a media's summary: ")
+			copies = input("Plase enter number of copies of media that will be stocked: ")
+            sq.Add_Media(dewey_dec,summary,Media_name,1,copies,null,null)
+			Director = input("Plase enter the dvd's Director: ") 
+				Genre = input("Plase enter the dvd's genre: ")
+				sq.Add_Media_dvd(Director,Genre,dewey_dec)
+		case 6:
+			id = input("Please enter your librarian ID: ")
+            #if ID in librarian IDs
+            Media_name = input("Plase enter a media's title/name: ")
+			dewey_dec = input("Plase enter a media's dewey decimal code: ")
+			summary = input("Plase enter a media's summary: ")
+			copies = input("Plase enter number of copies of media that will be stocked: ")
+            sq.Add_Media(dewey_dec,summary,Media_name,1,copies,null,null)
+			sq.Add_Media_item(dewey_dec)
+
 
 
 def Show_Tables():
